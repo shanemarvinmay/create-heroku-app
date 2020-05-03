8295b650e7c51f6c9ffa0fc9e38eca6ce1a8762f
shanemarvinmay <shanemarvinmay@gmail.com>
2020-05-03 09:11:25 -0500

    able to run commands done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-const {Command, flags} = require('@oclif/command')
+const {Command, flags} = require('@oclif/command');
+const { exec } = require("child_process");
+
+exec("ls", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
 
 class CreateHerokuAppCommand extends Command {
   async run() {
