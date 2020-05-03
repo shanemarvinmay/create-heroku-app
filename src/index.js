72bbaba009475f7508770706cb19b28d61cd0992
shanemarvinmay <shanemarvinmay@gmail.com>
2020-05-03 10:54:01 -0500

    npm init and express install done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const {Command, flags} = require('@oclif/command');
+const { Command, flags } = require('@oclif/command');
 const { exec } = require("child_process");
+const fs = require('fs');
 // how to run commands 
 // exec("ls", (error, stdout, stderr) => {
 //   if (error) {
@@ -12,35 +13,64 @@ const { exec } = require("child_process");
 //   }
 //   console.log(`stdout: ${stdout}`);
 // });
-// how to check for a module 
-try {
-  console.log(require.resolve("express"));
-} catch(e) {
-  console.log('Express is not installed.\nInstalling now...');
-  exec("npm install express", (error, stdout, stderr) => {
+
+const checkArgs = (name, type) => {
+  let error = null;
+  if (name == 'Unknown') {
+    error = `Error: App name unkown.\n`;
+  }
+  if (type == 'Unknown') {
+    error += `Error: App type unkown.\n`;
+  }
+  return error;
+};
+const expressInit = async (name) => {
+  // how to check for a express 
+  exec(`cd ../${name} && npm init -y && npm install express`, (error, stdout, stderr) => {
     if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+      console.log(`error: ${error.message}`);
+      return;
     }
     if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+      console.log(`stderr: ${stderr}`);
+      return;
     }
     console.log(`stdout:\n ${stdout}`);
   });
 }
-
-class CreateHerokuAppCommand extends Command {
-  async run() {
-    const {flags} = this.parse(CreateHerokuAppCommand)
-    // const name = flags.name || 'world';
-    const typeOfApp = flags.type || 'Unknown';
-    if(typeOfApp == 'Unknown'){
-      this.log(`Error: App type unkown.`);
+const makeProject = async (name) => {
+  exec(`cd .. && mkdir ${name} && cd ${name}`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
       return;
     }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+class CreateHerokuAppCommand extends Command {
+  async run() {
+    const { flags } = this.parse(CreateHerokuAppCommand)
+    const name = flags.name || 'Unknown';
+    const typeOfApp = flags.type || 'Unknown';
+    // checking for errors 
+    const error = checkArgs(name, typeOfApp);
+    if (error != null) {
+      this.log(error);
+      return;
+    }
+    // making project 
+    try {
+      await makeProject(name);
+    } catch (e) {
+      console.log('proj already exist');
+    }
+    await expressInit(name);
     this.log(`App type selected: ${typeOfApp}`);
-    // this.log(`hello ${name} from ./src/index.js`);
+    this.log(`App name given: ${name}`);
   }
 }
 
@@ -51,11 +81,11 @@ Extra documentation goes here
 
 CreateHerokuAppCommand.flags = {
   // add --version flag to show CLI version
-  version: flags.version({char: 'v'}),
+  version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
-  help: flags.help({char: 'h'}),
-  // name: flags.string({char: 'n', description: 'name to print'}),
-  type: flags.string({char: 't', description: 'type of app'}),
+  help: flags.help({ char: 'h' }),
+  name: flags.string({ char: 'n', description: 'name of project' }),
+  type: flags.string({ char: 't', description: 'type of app' }),
 }
 
 module.exports = CreateHerokuAppCommand
